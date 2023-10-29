c3a054d9111373798334151c93e4acad9673411d
101JamesLi <lizhengxi1216@163.com>
2023-10-29 21:44:08 +0800

    first test

--- ex43/ex43_classes.py
+++ ex43/ex43_classes.py
@@ -6,10 +6,14 @@ class Scene(object):
 class Engine(object):
 
     def __init__(self, game_map):
-        pass
+        self.game_map = game_map
+        last_scene = self.game_map.next_scene("Death")
 
     def play(self):
-        pass
+        while current_scene != self.last_scene:
+            next_scene_name = current_scene.enter()
+            current_scene = self.game_map.next_scene(next_scene_name)
+
 
 class Death(Scene):
 
@@ -38,15 +42,21 @@ class EscapePod(Scene):
 
 class Map(object):
 
+    Scenes = {
+        "Death": Death(),
+        "CentralCorridor": CentralCorridor(),
+        "TheBridge": TheBridge()
+    }
+
     def __init__(self, start_scene):
-        pass
+        self.start_scene = start_scene
 
     def next_scene(self, scene_name):
-        pass
+        return self.Scenes.get(scene_name)
 
     def opening_scene(self):
         pass
 
-a_map = Map("CentralCorridor")
-a_game = Engine(a_map)
-a_game.play()
+# a_map = Map("CentralCorridor")
+# a_game = Engine(a_map)
+# a_game.play()
