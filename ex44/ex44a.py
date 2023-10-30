1b9f298e5c1e166533553101592feeee4e441d16
JamesLi <lizhengxi1216@163.com>
2023-10-30 20:26:06 +0800

    ex44

---
+++ ex44/ex44a.py
@@ -0,0 +1,14 @@
+# implicit inheritance
+
+class Parent(object):
+
+    def implicit(self):
+        print("Parent implicit()")
+
+class Child(Parent):
+    pass
+
+father = Parent()
+son = Child()
+father.implicit()
+son.implicit()
---
+++ ex44/ex44b.py
@@ -0,0 +1,17 @@
+# Override inheritance
+
+class Parent(object):
+
+    def override(self):
+        print("PARENT override()")
+
+class Child(Parent):
+
+    def override(self):
+
+        print("CHILD override()")
+
+pap = Parent()
+son = Child()
+pap.override()
+son.override()
